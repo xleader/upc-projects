284bb994fe9d78734d5a9690186db0d7b8bac699
xleader <leojay1@gmail.com>
2016-08-11 21:21:30 +0800

    继续构建项目

---
+++ upc-test/src/main/java/com/leojay1/upc/test/App.java
@@ -0,0 +1,13 @@
+package com.leojay1.upc.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
